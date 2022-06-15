3f620204c33e613ab64f8d3ce1e6007786e2c184
Alok Sarkar <aloksarkar0002@gmail.com>
2022-06-15 21:39:59 +0530

    [ADD] Exception Handling & coloring

--- src/NumberSystemConverter.java
+++ src/NumberSystemConverter.java
@@ -1,26 +1,21 @@
-import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class NumberSystemConverter {
     static final String ANSI_GREEN = "\u001B[32m";
     static final String ANSI_RESET = "\u001B[0m";
-    static final String ANSI_YELLOW = "\u001B[33m";
     static final String ANSI_RED = "\u001B[31m";
-    static final String ANSI_BLUE = "\u001B[34m";
     static final String ANSI_PURPLE = "\u001B[35m";
-    static final String ANSI_CYAN = "\u001B[36m";
     public static void main(String[] args) {
-        System.out.println("                                      Number Base converter");
-        System.out.println("                                   ===========================");
-        System.out.println("                     [1] (Dec)imal, [2] (Bin)ary, [3] (Hex)adecimal, [4] (Oct)al\n");
+        System.out.println(ANSI_GREEN+"                                      Number Base converter"+ANSI_RESET);
+        System.out.println(ANSI_GREEN+"                                   ==========================="+ANSI_RESET);
+        System.out.println(ANSI_GREEN+"                     [1] (Dec)imal, [2] (Bin)ary, [3] (Hex)adecimal, [4] (Oct)al\n"+ANSI_RESET);
 
         int currentBase= getBase("Number system to convert from: ");
 
         String number= getValidNumber(currentBase);
         int requiredBase=getBase("Number system to convert to: ");
-        String convertednumber=convert(number,currentBase,requiredBase);
-        System.out.println("Converted number: "+convertednumber);
+        String convertedNumber =convert(number,currentBase,requiredBase);
+        System.out.println("Converted number: "+ convertedNumber);
     }
     static String getValidNumber(int base){
         Scanner scan= new Scanner(System.in);
@@ -40,7 +35,6 @@ public class NumberSystemConverter {
     }
     static boolean valid(String number, int base){
         boolean result=false;
-        System.out.println("Checking for base: "+base+" & number: "+number);
         switch (base) {
             case 10 -> result = number.matches("^[0-9]+$");
             case 2 -> result = number.matches("^[0-1]+$");
@@ -50,12 +44,12 @@ public class NumberSystemConverter {
         }
         return result;
     }
-    static int getBase(String promt){
+    static int getBase(String prompt){
         int choice;
         Scanner scan= new Scanner(System.in);
 
         choiceTaken: while (true){
-            System.out.print(ANSI_YELLOW+promt+ANSI_RESET);
+            System.out.print(ANSI_PURPLE+ prompt +ANSI_RESET);
             String userChoice =scan.nextLine().toLowerCase();
             switch (userChoice){
                 case "1":
@@ -88,19 +82,24 @@ public class NumberSystemConverter {
     }
     static String convert(String number, int currentBase, int requiredBase){
         int decimalValue=0;
-        if(currentBase==10){
-            decimalValue=Integer.parseInt(number);
-        }else if (currentBase==2){
-            decimalValue=Integer.parseInt(number,2);
+        try{
+            if(currentBase==10){
+                decimalValue=Integer.parseInt(number);
+            }else if (currentBase==2){
+                decimalValue=Integer.parseInt(number,2);
 
-        } else if (currentBase==8) {
-            decimalValue=Integer.parseInt(number,8);
-        }else if(currentBase==16){
-            decimalValue=Integer.parseInt(number,16);
-        }else {
-            System.out.println(ANSI_RED+"Invalid base..... Should not have happened"+ANSI_RESET);
-            return "";
+            } else if (currentBase==8) {
+                decimalValue=Integer.parseInt(number,8);
+            }else if(currentBase==16){
+                decimalValue=Integer.parseInt(number,16);
+            }else {
+                System.out.println(ANSI_RED+"Invalid base..... Should not have happened"+ANSI_RESET);
+                return "";
+            }
+        }catch (NumberFormatException e){
+            System.out.println("Invalid number");
         }
+
         if(requiredBase==2){
             return Integer.toBinaryString(decimalValue);
         }else if(requiredBase==8){
